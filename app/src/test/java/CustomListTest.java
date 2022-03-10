1ba5a87d1f80f08852303177f0d0a433a3c8ada4
AYUSH ROY <ayush3@ualberta.ca>
2022-03-10 00:42:27 -0700

    Test

---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+    private CustomList list;
+
+    @BeforeAll
+    public void createList(){
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("Edmonton", "AB"));
+        assertEquals(list.getCount(), listSize + 1);
+        
+
+    }
+
+
+}
