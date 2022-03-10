6602dc6dea3d4d90b9fd0b0f7d38c1cf9c9747ef
AYUSH ROY <ayush3@ualberta.ca>
2022-03-10 12:37:12 -0700

    Test

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -71,6 +71,7 @@ public class CustomList extends ArrayAdapter<City> {
         for(City place:cities){
             if (place.getCityName() == city.getCityName()){
                 cities.remove(place);
+                return;
             }
 
         }
